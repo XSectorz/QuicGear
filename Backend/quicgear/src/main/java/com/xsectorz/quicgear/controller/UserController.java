4d991966e77c520454bbe8480d6dbeb7af023158
XSectorZ <panatschool@gmail.com>
2025-04-05 14:00:46 +0700

    Init first user api endpoint

---
+++ Backend/quicgear/src/main/java/com/xsectorz/quicgear/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.xsectorz.quicgear.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xsectorz.quicgear.model.User;
+import com.xsectorz.quicgear.repository.UserRepository;
+
+
+@RestController
+@RequestMapping("/api/v1/")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    //Get all user
+    @GetMapping("/users")
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+
+
+}
